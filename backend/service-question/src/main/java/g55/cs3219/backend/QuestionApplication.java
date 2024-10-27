74dce9e99a98ac96a17aa7b034b2584fadc5b5e3
jeff <jefferyng2000@gmail.com>
2024-10-27 13:23:49 +0800

    feat: Enable discovery client for question service

--- backend/service-question/src/main/java/g55/cs3219/backend/QuestionApplication.java
+++ backend/service-question/src/main/java/g55/cs3219/backend/QuestionApplication.java
@@ -2,8 +2,11 @@ package g55.cs3219.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class QuestionApplication {
 
 	public static void main(String[] args) {
