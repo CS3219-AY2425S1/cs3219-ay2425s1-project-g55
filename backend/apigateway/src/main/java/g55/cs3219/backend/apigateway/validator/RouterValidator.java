43c8c3147d379bffc8e9e0a2bb7e965accc20049
jeff <jefferyng2000@gmail.com>
2024-10-30 11:06:05 +0800

    feat: Add route validator

---
+++ backend/apigateway/src/main/java/g55/cs3219/backend/apigateway/validator/RouterValidator.java
@@ -0,0 +1,22 @@
+package g55.cs3219.backend.apigateway.validator;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RouterValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/api/auth",
+            "/eureka"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
