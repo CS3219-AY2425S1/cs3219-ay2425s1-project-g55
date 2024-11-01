fc073198000512515ab0e33b05eb0841d3b2f89e
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-11-01 23:44:19 +0800

    refactor: update package name

--- backend/service-history/src/main/java/g55/cs3219/backend/ServiceHistoryApplication.java
+++ backend/service-history/src/main/java/g55/cs3219/backend/ServiceHistoryApplication.java
@@ -1,4 +1,4 @@
-package g55.cs3219.backend.service_history;
+package g55.cs3219.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
