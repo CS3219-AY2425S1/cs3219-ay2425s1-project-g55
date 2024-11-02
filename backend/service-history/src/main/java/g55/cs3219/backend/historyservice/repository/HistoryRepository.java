3a1640959a449a770c43628581d3508a51a1217d
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-11-02 12:43:24 +0800

    refactor: update HistoryRepository in service-history module

--- backend/service-history/src/main/java/g55/cs3219/backend/historyservice/repository/HistoryRepository.java
+++ backend/service-history/src/main/java/g55/cs3219/backend/historyservice/repository/HistoryRepository.java
@@ -6,10 +6,9 @@ import org.springframework.stereotype.Repository;
 import g55.cs3219.backend.historyservice.model.History;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface HistoryRepository extends MongoRepository<History, String> {
     List<History> findByUserIdOrderByAttemptDateTimeDesc(String userId);
-    Optional<History> findByUserIdAndQuestionId(String userId, String questionId);
+    List<History> findByUserIdAndQuestionIdOrderByAttemptDateTimeDesc(String userId, String questionId);
 }
