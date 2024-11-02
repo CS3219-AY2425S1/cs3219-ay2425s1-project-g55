a6c13f5a9c780507e9f3d5385c4dd5e587962be6
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-11-02 12:43:19 +0800

    refactor: update HistoryService in service-history module

--- backend/service-history/src/main/java/g55/cs3219/backend/historyservice/service/HistoryService.java
+++ backend/service-history/src/main/java/g55/cs3219/backend/historyservice/service/HistoryService.java
@@ -5,7 +5,6 @@ import g55.cs3219.backend.historyservice.repository.HistoryRepository;
 import g55.cs3219.backend.historyservice.model.History;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 @Service
@@ -14,19 +13,23 @@ public class HistoryService {
     @Autowired
     private HistoryRepository historyRepository;
 
+    public History createHistory(History history) {
+        return historyRepository.save(history);
+    }
+    
+    public List<History> getHistoryByUserIdAndQuestionId(String userId, String questionId) {
+        return historyRepository.findByUserIdAndQuestionIdOrderByAttemptDateTimeDesc(userId, questionId);
+    }
+    
     public List<History> getHistoryByUserId(String userId) {
         return historyRepository.findByUserIdOrderByAttemptDateTimeDesc(userId);
     }
 
-    public void saveHistory(History history) {
-        historyRepository.save(history);
+    public History updateHistory(String id, History history) {
+        return historyRepository.save(history);
     }
 
     public void deleteHistory(String id) {
         historyRepository.deleteById(id);
-    }
-
-    public Optional<History> getHistoryByUserIdAndQuestionId(String userId, String questionId) {
-        return historyRepository.findByUserIdAndQuestionId(userId, questionId);
     }
 }
