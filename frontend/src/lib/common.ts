2ed180657577e55db6270d3052c14774546cee0b
m1oojv <97022614+m1oojv@users.noreply.github.com>
2024-11-02 13:24:20 +0800

    feat: Change frontend api calls to api gateway

--- frontend/src/lib/common.ts
+++ frontend/src/lib/common.ts
@@ -1,9 +1,12 @@
-export const BACKEND_URL_USER_BASE = "http://localhost:8081";
-export const BACKEND_URL_AUTH = `${BACKEND_URL_USER_BASE}/api/auth`;
-export const BACKEND_URL_USERS = `${BACKEND_URL_USER_BASE}/api/users`;
+export const BACKEND_URL_BASE = "http://localhost:8080";
+export const BACKEND_WS_BASE = "ws://localhost:8080";
 
-export const BACKEND_URL_ROOM = 'http://localhost:8083/api/rooms';
-export const BACKEND_WEBSOCKET_ROOM = 'ws://localhost:8083/ws/rooms';
+export const BACKEND_URL_AUTH = `${BACKEND_URL_BASE}/api/auth`;
+export const BACKEND_URL_USERS = `${BACKEND_URL_BASE}/api/users`;
+export const BACKEND_URL_QUESTIONS = `${BACKEND_URL_BASE}/api/questions`;
 
-export const BACKEND_URL_MATCHING = 'http://localhost:8082/api/matching';
-export const BACKEND_WEBSOCKET_MATCHING = 'ws://localhost:8082/ws/matching';
+export const BACKEND_URL_ROOM = `${BACKEND_URL_BASE}/api/rooms`;
+export const BACKEND_WEBSOCKET_ROOM = `${BACKEND_WS_BASE}/ws/rooms`;
+
+export const BACKEND_URL_MATCHING = `${BACKEND_URL_BASE}/api/matching`;
+export const BACKEND_WEBSOCKET_MATCHING = `${BACKEND_WS_BASE}/ws/matching`;
