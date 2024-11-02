5befff5d6f276b08d2b1325a044201f07086f05a
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-11-02 15:53:05 +0800

    refactor: Update QuestionRoute to use tabs for displaying question and submissions

--- frontend/src/routes/question.tsx
+++ frontend/src/routes/question.tsx
@@ -7,6 +7,8 @@ import {
 import CodeEditor from '@/components/code-editor';
 import QuestionView from '@/components/QuestionView';
 import { useParams } from 'react-router-dom';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import SubmissionView from '@/components/SubmissionView';
 
 export default function QuestionRoute() {
   const { questionId: questionIdString } = useParams<{ questionId: string }>();
@@ -20,7 +22,22 @@ export default function QuestionRoute() {
     <div className="mx-4 mb-4 border rounded-lg overflow-hidden w-full h-full">
       <ResizablePanelGroup direction="horizontal">
         <ResizablePanel defaultSize={40}>
-          <QuestionView id={questionId} />
+          <Tabs defaultValue="question">
+            <TabsList className="w-full">
+              <TabsTrigger value="question" className="flex-1">
+                Question
+              </TabsTrigger>
+              <TabsTrigger value="submissions" className="flex-1">
+                Submissions
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="question">
+              <QuestionView id={questionId} />
+            </TabsContent>
+            <TabsContent value="submissions">
+              <SubmissionView id={questionId} />
+            </TabsContent>
+          </Tabs>
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel>
