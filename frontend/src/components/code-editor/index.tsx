875e9897db105d17bc6eaaec51f9117ea9124c97
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-11-02 17:32:37 +0800

    refactor: Update CodeEditor component to include value prop
    This commit updates the CodeEditor component to include a new prop called 'value'. This prop allows the parent component to control the value of the code editor. The 'value' prop is now passed to the Monaco Editor component and is used to set the initial value of the editor. Additionally, the 'value' prop is also passed to the onChange event handler, ensuring that any changes made in the editor are reflected in the parent component.

--- frontend/src/components/code-editor/index.tsx
+++ frontend/src/components/code-editor/index.tsx
@@ -1,20 +1,23 @@
 import Editor from '@monaco-editor/react';
 
 const CodeEditor = ({
-  initialValue,
+  initialValue = '// Write your code here\n',
+  value,
   onChange,
 }: {
-  initialValue: string;
+  initialValue?: string;
+  value: string;
   onChange: (value: string) => void;
 }) => {
   return (
-      <Editor
-        defaultLanguage='javascript'
-        defaultValue={initialValue}
-        onChange={(value: string | undefined) => {
-          if (value) onChange(value);
-        }}
-      />
+    <Editor
+      defaultLanguage="javascript"
+      defaultValue={initialValue}
+      value={value}
+      onChange={(value: string | undefined) => {
+        if (value) onChange(value);
+      }}
+    />
   );
 };
 
