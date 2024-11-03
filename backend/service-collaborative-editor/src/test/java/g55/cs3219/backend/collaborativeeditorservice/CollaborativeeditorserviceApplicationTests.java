56769ddcd510af495e97c261e76884aea8b22480
zsheng <zsheng.dev@gmail.com>
2024-11-03 19:01:54 +0800

    Add collaborative editor servcie boilerplate

---
+++ backend/service-collaborative-editor/src/test/java/g55/cs3219/backend/collaborativeeditorservice/CollaborativeeditorserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package g55.cs3219.backend.collaborativeeditorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeEditorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
