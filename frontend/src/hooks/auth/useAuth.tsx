1692a148f663a53a8ab19d559b7344d55446eaf1
zsheng <zsheng.dev@gmail.com>
2024-11-08 23:14:48 +0800

    Invalidate auth after updating user

--- frontend/src/hooks/auth/useAuth.tsx
+++ frontend/src/hooks/auth/useAuth.tsx
@@ -1,4 +1,9 @@
-import { LOCAL_STORAGE_KEYS, LoginResponseSchema } from '@/types/auth';
+import { BACKEND_URL_AUTH } from '@/lib/common';
+import {
+  LOCAL_STORAGE_KEYS,
+  LoginResponseSchema,
+  VerifyTokenResponseSchema,
+} from '@/types/auth';
 import { jwtDecode } from 'jwt-decode';
 import { useEffect, useState } from 'react';
 
@@ -15,6 +20,7 @@ const USER_ROLES = {
 type AuthHelper = {
   user: AuthUser;
   logout: () => void;
+  refreshAuth: () => Promise<void>;
 };
 
 type AuthUser = {
@@ -70,6 +76,48 @@ export function useAuth(): AuthHelper | null {
     // You might want to set up a timer to periodically check the token's validity
   }, []);
 
+  const refreshAuth = async () => {
+    const loginResponse = LoginResponseSchema.safeParse(
+      JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEYS.USER) || '{}')
+    );
+
+    if (!loginResponse.success) {
+      clearAuthData();
+      setUser(null);
+      return;
+    }
+
+    const response = await fetch(`${BACKEND_URL_AUTH}/verify-token`, {
+      headers: {
+        Authorization: `Bearer ${loginResponse.data.token}`,
+      },
+    });
+
+    if (!response.ok) {
+      clearAuthData();
+      setUser(null);
+    }
+
+    const data = await response.json();
+    const user = VerifyTokenResponseSchema.parse(data);
+    setUser({
+      role: user.admin ? USER_ROLES.admin : USER_ROLES.user,
+      userId: user.id,
+      userName: user.username,
+      email: user.email,
+    });
+    localStorage.setItem(
+      LOCAL_STORAGE_KEYS.USER,
+      JSON.stringify({
+        id: user.id,
+        token: loginResponse.data.token,
+        email: user.email,
+        username: user.username,
+        admin: user.admin,
+      })
+    );
+  };
+
   return user
     ? {
         user,
@@ -78,6 +126,7 @@ export function useAuth(): AuthHelper | null {
           setUser(null);
           window.location.reload();
         },
+        refreshAuth,
       }
     : null;
 }
--- frontend/src/types/auth.ts
+++ frontend/src/types/auth.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const RegisterUserSchema = z
   .object({
@@ -8,14 +8,14 @@ export const RegisterUserSchema = z
       .min(8)
       .regex(
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/,
-        "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character. Special characters include @ $ ! % * ? &"
+        'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character. Special characters include @ $ ! % * ? &'
       ),
     confirmPassword: z.string().min(8),
     username: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
-    path: ["confirmPassword"],
+    path: ['confirmPassword'],
   });
 
 export type RegisterUser = z.infer<typeof RegisterUserSchema>;
@@ -26,6 +26,19 @@ export const LoginUserSchema = z.object({
 });
 
 export type LoginUser = z.infer<typeof LoginUserSchema>;
+
+
+/**
+ * Schema for the /api/auth/verify-token endpoint
+ */
+export const VerifyTokenResponseSchema = z.object({
+  id: z.number(),
+  email: z.string().email(),
+  username: z.string(),
+  admin: z.boolean(),
+});
+
+export type VerifyTokenResponse = z.infer<typeof VerifyTokenResponseSchema>;
 
 export const LoginResponseSchema = z.object({
   id: z.number(),
@@ -54,37 +67,37 @@ export const ChangePasswordSchema = z
     oldPassword: z.string(),
     newPassword: z
       .string()
-      .min(8, "New password must be at least 8 characters"),
+      .min(8, 'New password must be at least 8 characters'),
     confirmPassword: z
       .string()
-      .min(8, "Confirm password must be at least 8 characters"),
+      .min(8, 'Confirm password must be at least 8 characters'),
   })
   .refine((data) => data.newPassword === data.confirmPassword, {
-    message: "New passwords do not match",
-    path: ["confirmPassword"],
+    message: 'New passwords do not match',
+    path: ['confirmPassword'],
   });
 
 export type ChangePasswordData = z.infer<typeof ChangePasswordSchema>;
 
 export const ForgotPasswordSchema = z.object({
-  email: z.string().email("Invalid email address"),
+  email: z.string().email('Invalid email address'),
 });
 
 export type ForgotPasswordData = z.infer<typeof ForgotPasswordSchema>;
 
 export const ResetPasswordSchema = z
   .object({
-    resetCode: z.string().min(6, "Reset code must be at least 6 characters"),
+    resetCode: z.string().min(6, 'Reset code must be at least 6 characters'),
     newPassword: z
       .string()
-      .min(8, "New password must be at least 8 characters"),
+      .min(8, 'New password must be at least 8 characters'),
     confirmPassword: z
       .string()
-      .min(8, "Confirm password must be at least 8 characters"),
+      .min(8, 'Confirm password must be at least 8 characters'),
   })
   .refine((data) => data.newPassword === data.confirmPassword, {
-    message: "New passwords do not match",
-    path: ["confirmPassword"],
+    message: 'New passwords do not match',
+    path: ['confirmPassword'],
   });
 
 export type ResetPasswordData = z.infer<typeof ResetPasswordSchema> & {
@@ -95,5 +108,5 @@ export type ResetPasswordData = z.infer<typeof ResetPasswordSchema> & {
  * Keys for auth related data in local storage
  */
 export const LOCAL_STORAGE_KEYS = {
-  USER: "user",
+  USER: 'user',
 } as const;
